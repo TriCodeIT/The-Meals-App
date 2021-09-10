4a7a57e5c7f3a3aa4b63aae98ba342205eea1852
TriCodeIT <bayu165kusuma@gmail.com>
2021-09-10 09:45:01 +0700

    Installing React Navigation & Adding Navigation to the App

---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const MealsNavigator = (props) => {
+  return (
+    <View>
+      <Text>Meals Navigator</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default MealsNavigator;
