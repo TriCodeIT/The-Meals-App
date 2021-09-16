e24b2673de96786011549a6f304ca19c06cf9758
TriCodeIT <bayu165kusuma@gmail.com>
2021-09-16 10:43:38 +0700

    Setting Dynamic Navigation Options

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,26 +1,47 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Platform } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const CategoryMealsScreen = (props) => {
-   const catId = props.navigation.getParam('categoryId')
+  const catId = props.navigation.getParam("categoryId");
 
-   const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
       <Text>{selectedCategory.title}</Text>
-      <Button title="Go to Details" onPress={() => {
-        props.navigation.navigate({
-          routeName: 'MealDetail'
-        })
-      }}/>
-      <Button title="Go Back" onPress={() => {
-        props.navigation.pop()
-      }} />
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          props.navigation.navigate({
+            routeName: "MealDetail",
+          });
+        }}
+      />
+      <Button
+        title="Go Back"
+        onPress={() => {
+          props.navigation.pop();
+        }}
+      />
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam("categoryId");
+
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
+  };
 };
 
 const styles = StyleSheet.create({
@@ -31,4 +52,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CategoryMealsScreen
+export default CategoryMealsScreen;
